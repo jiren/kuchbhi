68aec66d85d3ee270f137713523846e25ed89542
sivagollapalli <siva@joshsoftware.com>
2012-10-13 07:21:57 +0530

    Added image storage path
    - seed data for categories

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,9 @@ class Image
   include Mongoid::Document
   include Mongoid::Paperclip
 
-  has_mongoid_attached_file :image
+  has_mongoid_attached_file :image,
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename"
 
   field :name, type: String
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = ['Four Wheeler', 'Two Wheeler', 'Home', 'Accessories', 'Real Estate']
+
+puts "Creating categories ...."
+categories.each {|c| Category.create!(:name => c) }
+puts "Done!!!!!"
