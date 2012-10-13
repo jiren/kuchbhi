fa36360f781b3e8f686d65f54f12e088807a739a
sivagollapalli <siva@joshsoftware.com>
2012-10-13 06:07:29 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuchbhi::Application.config.secret_token = '9cfb9093dca3418ca1fef5aaf52e1eb5b9d8dad798b7bb1a5dca8d3f64bd7f4d4cdaae271936cfb25aefd8f8a055fff4e17759aab1475a373d1a358cdd5fb043'
